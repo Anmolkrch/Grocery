af87c6abd06b6b552c270210f0810001325baf33
Anmolkrch <brajeshkr18@gmail.com>
2026-06-28 11:50:42 +0530

    change name

---
+++ OnlineShopping/Controllers/MasterController.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json;
+using OnlineShopping.DAL;
+using OnlineShopping.Filters;
+using OnlineShopping.Models;
+using OnlineShopping.Repository;
+using OnlineShopping.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    [AuthorizeUser(Roles = "Admin")]
+    public class MasterController : Controller
+    {
+        #region Other Class references ...
+        // Instance on Unit of Work
+        private GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        #endregion
+
+
+        /// <summary>
+        /// Categories
+        /// </summary>
+        /// <returns></returns>
+        #region Manage Categories ...
+        public ActionResult Cities()
+        {
+            var allCities = _unitOfWork.GetRepositoryInstance<Tbl_City>()
+                .GetAllRecordsIQueryable().Include(x=>x.Tbl_State)
+                .Where(i => i.IsDelete == false)
+                .Select(c => new CityViewModel
+                {
+                    CityId = c.CityId,
+                    StateId = c.StateId,
+                    CityName = c.CityName,
+                    IsActive = c.IsActive,
+                    IsDelete = c.IsDelete,
+                    CreatedDate = c.CreatedDate,
+                    StateName = c.Tbl_State.StateName,
+                })
+                .ToList();
+
+            return View(allCities);
+        }
+
+
+        /// <summary>
+        /// Add Category
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddCity()
+        {
+            return UpdateCity(0);
+        }
+
+        /// <summary>
+        /// Update Category
+        /// </summary>
+        /// <param name="categoryId"></param>
+        /// <returns></returns>
+        public ActionResult UpdateCity(int cityId)
+        {
+            var states = _unitOfWork
+           .GetRepositoryInstance<Tbl_State>()
+           .GetAllRecordsIQueryable()
+           .Where(x => x.IsActive && !x.IsDelete)
+           .Select(x => new SelectListItem
+           {
+               Value = x.StateId.ToString(),
+               Text = x.StateName
+           })
+           .ToList();
+            var countries = _unitOfWork
+           .GetRepositoryInstance<Tbl_Country>()
+           .GetAllRecordsIQueryable()
+           .Where(x => x.IsActive && !x.IsDelete)
+           .Select(x => new SelectListItem
+           {
+               Value = x.CountryId.ToString(),
+               Text = x.CountryName
+           })
+           .ToList();
+            CityViewModel cd;
+            if (cityId != 0)
+            {
+                var category = _unitOfWork
+                    .GetRepositoryInstance<OnlineShopping.DAL.Tbl_City>()
+                    .GetFirstOrDefault(cityId);
+
+                cd = new CityViewModel
+                {
+                    CityId = category.CityId,
+                    CityName = category.CityName,
+                    StateId = category.StateId,
+                    IsActive = category.IsActive,
+                    States = states,
+                    Countries = countries
+                };
+            }
+            else
+            {
+                cd = new CityViewModel
+                {
+                    States = states,
+                    Countries = countries
+                };
+            }
+
+            return View("UpdateCity", cd);
+        }
+
+
+        /// <summary>
+        /// Update Category
+        /// </summary>
+        /// <param name="cd"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateCity(CityViewModel cd)
+        {
+            if (ModelState.IsValid)
+            {
+                // Fetch existing city
+                Tbl_City city = _unitOfWork
+                    .GetRepositoryInstance<Tbl_City>()
+                    .GetFirstOrDefault(cd.CityId);
+
+                // If not found, create new
+                if (city == null)
+                {
+                    city = new Tbl_City
+                    {
+                        CreatedDate = DateTime.Now,
+                        IsActive = true,
+                        IsDelete = false,
+                    };
+                }
+
+                // Update fields
+                city.CityName = cd.CityName;
+                city.StateId = cd.StateId;
+                city.IsActive = cd.IsActive;
+                city.IsDelete = cd.IsDelete;
+
+                _unitOfWork.GetRepositoryInstance<Tbl_City>().Add(city);
+                // Save changes
+                _unitOfWork.SaveChanges();
+
+                return RedirectToAction("Cities");
+            }
+
+            // If validation fails, return to view
+            return View("UpdateCity", cd);
+        }
+
+        #endregion
+
+
+        #region Disposing UnitOfWork Context ...
+        protected override void Dispose(bool disposing)
+        {
+            _unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+        #endregion
+    }
+}
---
+++ OnlineShopping/Models/CityViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Models
+{
+    public class CityViewModel
+    {
+        public int CityId { get; set; }
+
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+
+        public string CityName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public System.DateTime CreatedDate { get; set; }
+
+        public IEnumerable<SelectListItem> Countries { get; set; }
+        public IEnumerable<SelectListItem> States { get; set; }
+
+    }
+    public class StateViewModel
+    {
+
+
+        public int StateId { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string StateName { get; set; }
+
+        public string StateCode { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public System.DateTime CreatedDate { get; set; }
+
+        public IEnumerable<SelectListItem> Countries { get; set; }
+    }
+    public class CountryViewModel
+    {
+        public int CountryId { get; set; }
+
+        public string CountryName { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public System.DateTime CreatedDate { get; set; }
+    }
+
+}
